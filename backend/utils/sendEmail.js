27fde8de9a3f7bf25ef017f992c94e747ed3c08c
Ansh Gupta <anshg1948@gmail.com>
2026-03-10 14:18:07 +0530

    e

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -7,7 +7,9 @@ const sendEmail = async ({ to, subject, html }) => {
     try {
         // Use real Gmail SMTP transport
         const transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
